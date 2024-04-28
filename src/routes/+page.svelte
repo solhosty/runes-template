b73f4e83cbc349e466b54f9269ad54cbb390ec9c
hunter <104146303+solhosty@users.noreply.github.com>
2024-04-28 02:27:34 -0400

    update notes in root

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,16 +6,19 @@
 
 	let animal_hair = $state<boolean>(false);
 	let odor = $state<boolean>(false);
+	// This could be a non-$state variable, but I want to show the difference. 
+	// It would better fit a dynamic variable. 
 	let cost = $state<number>(basic_appointment.price);
+	// Since we need something that is dependent on animal_hair and odor, we will use $derived.
 	let derived_cost = $derived(cost + (animal_hair ? 10 : 0) + (odor ? 5 : 0));
 
 	// This will log the value of animal_hair to the console
 	// Only on init & update. 
 	$inspect('Animal Hair Value: ', animal_hair);
 	// Same thing with derived_cost. 
-	$inspect(derived_cost);
+	$inspect('Derived cost: ', derived_cost);
 	// The only thing that won't update is cost since that value does not change.
-	$inspect(cost);
+	$inspect('Initial cost: ', cost);
 </script>
 
 <section class="m-auto items-center max-w-3xl space-y-3 justify-center">
